6d4c01ec40d65d7066e130118ab209d9e0e0bcbc
Eazaz Jakda <80489515+EazazJ1@users.noreply.github.com>
2022-12-25 11:50:03 -0500

    Added frontend and backend projects starting point. Currently empty.

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+
+
+app.listen(3000, () => console.log('Server Started'))
