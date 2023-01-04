2b48a2c006c111aa20ba384f51ac4de90d3956a5
EazazJ1 <ejakda2584@conestogac.on.ca>
2023-01-04 15:08:07 -0500

    Everything working with nginx and certbot

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const path = require('path');
 const mongoose = require('mongoose')
-const cors = require("cors");
+const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login/new" element={<Register/>} />
-        <Route path="/passwords/new" element={check != null ? <AddPassword /> : <Homepage />} />
+        <Route path="/passwords/new" element={localStorage.getItem("user") ? <AddPassword /> : <Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<Mainpage />} />
         <Route path="/update" element={<EditForm />} />
