e01c46d8a0ad33c4b21aa78d4b907d1884f965d0
EazazJ1 <ejakda2584@conestogac.on.ca>
2023-01-05 12:11:24 -0500

    Fixed bug with add passwords button, and added text to homepage.

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -3,7 +3,8 @@ import "./homepage.css";
 
 const Homepage = () => {
 
-  return (<div><h1 className="homeTitle">Password Manager</h1></div>);
+  // return (<div><h1 className="homeTitle">Password Manager</h1></div>);
+  return (<div><h1 className="homeTitle">Password Manager</h1><br/><br/><h2 className="loginMsg">Login to manage passwords.</h2></div>);
 
 };
 
